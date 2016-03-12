ad47d8fd43bb3949c7ae1f43e27a3ab6fed204c2
Maciej Piekarski <s10739@pjwstk.edu.pl>
2016-03-12 16:27:58 +0100

    code refactor

--- psw/views.py
+++ psw/views.py
@@ -2,7 +2,6 @@
 Definition of views.
 """
 import paramiko
-
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
 from django.template import RequestContext
@@ -14,24 +13,22 @@ from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login
 from django import forms
 
+
 def ip_adding():
     ip_tuple = Commands.objects.values_list('ip')
     ip_list = [x[0] for x in ip_tuple]
-    ip_start = ['192.168.0.','10']
+    ip_start = ['192.168.0.', '10']
     ip_join = ip_start[0] + ip_start[1]
-    
 
     for item in ip_list:
         if item == ip_join:
-            a = int(ip_start[1])+1
+            a = int(ip_start[1]) + 1
             ip_start[1] = str(a)
             ip_join = ip_start[0] + ip_start[1]
-            
+
         else:
             ip_join
     return ip_join
-        
-        
 
 
 def home(request):
@@ -40,12 +37,13 @@ def home(request):
     return render(
         request,
         'psw/index.html',
-        context_instance = RequestContext(request,
-        {
-            'title':'Home Page',
-            'year':datetime.now().year,
-        })
+        context_instance=RequestContext(request,
+                                        {
+                                            'title': 'Home Page',
+                                            'year': datetime.now().year,
+                                        })
     )
+
 
 def contact(request):
     """Renders the contact page."""
@@ -53,13 +51,14 @@ def contact(request):
     return render(
         request,
         'psw/contact.html',
-        context_instance = RequestContext(request,
-        {
-            'title':'Contact',
-            'message':'Your contact page.',
-            'year':datetime.now().year,
-        })
+        context_instance=RequestContext(request,
+                                        {
+                                            'title': 'Contact',
+                                            'message': 'Your contact page.',
+                                            'year': datetime.now().year,
+                                        })
     )
+
 
 def about(request):
     """Renders the about page."""
@@ -67,13 +66,14 @@ def about(request):
     return render(
         request,
         'psw/about.html',
-        context_instance = RequestContext(request,
-        {
-            'title':'About',
-            'message':'Your application description page.',
-            'year':datetime.now().year,
-        })
+        context_instance=RequestContext(request,
+                                        {
+                                            'title': 'About',
+                                            'message': 'Your application description page.',
+                                            'year': datetime.now().year,
+                                        })
     )
+
 
 def register(request):
     if request.method == 'POST':
@@ -83,6 +83,7 @@ def register(request):
             return HttpResponse('poszło')
     form = pswCreateForm()
     return render(request, 'psw/register.html', {'form': form})
+
 
 def login_view(request):
     if request.method == 'POST':
@@ -98,63 +99,62 @@ def login_view(request):
             return HttpResponse('Nie poszło - złe dane')
     return render(request, 'psw/login.html', {'form': pswAuthenticationForm(request.POST)})
 
+
 def servers(request):
     if request.method == 'POST':
         form = CommandForm(request.POST)
-        
+
         if form.is_valid():
             form.instance.user = request.user
             ip = ip_adding()
             form.instance.ip = ip
             name = form.cleaned_data['name']
-            #ip = form.cleaned_data['ip']
+            # ip = form.cleaned_data['ip']
             system = form.cleaned_data['system']
             ram = form.cleaned_data['ram']
             quote = form.cleaned_data['quote']
             username = str(request.user.get_username())
-            #commandlog = 'python3.5 /root/log_skrypt.py'+ ' '+ ip + ' ' + system + ' ' + ram + ' ' + quote +  ' ' + username + name + ' >> PSW_log.log'
-            #command = 'python3.5 /root/main_skrypt.py'+ ' '+ ip + ' ' + system + ' ' + ram + ' ' + quote + ' ' + username + name +'  > wyniki_testy.txt'
+            # commandlog = 'python3.5 /root/log_skrypt.py'+ ' '+ ip + ' ' + system + ' ' + ram + ' ' + quote +  ' ' + username + name + ' >> PSW_log.log'
+            # command = 'python3.5 /root/main_skrypt.py'+ ' '+ ip + ' ' + system + ' ' + ram + ' ' + quote + ' ' + username + name +'  > wyniki_testy.txt'
             form.save()
 
-            #Tworzenie ze skryptu.py Python 3.5 
+            # Tworzenie ze skryptu.py Python 3.5
             try:
                 ssh = paramiko.SSHClient()
                 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
                 ssh.connect('89.206.7.46', username='root', password='TrudneHaslo123')
                 # Tworzenie Log
-                #stdin, stdout, stderr = ssh.exec_command(commandlog)
+                # stdin, stdout, stderr = ssh.exec_command(commandlog)
                 # Tworzenie kontenerow
-                #stdin, stdout, stderr = ssh.exec_command(command)
+                # stdin, stdout, stderr = ssh.exec_command(command)
                 ssh.close()
             except paramiko.ssh_exception.NoValidConnectionsError as e:
-                print ('Error %s' %e)
+                print('Error %s' % e)
                 return HttpResponseRedirect('servers/')
             return HttpResponseRedirect('/')
     else:
-         form = CommandForm()
+        form = CommandForm()
     return render(request, 'psw/servers.html', {'form': form})
 
+
 def listservers(request):
-    
     servers = Commands.objects.filter(user=request.user)
     context_dict = {'servers': servers}
-    return render(request, 'psw/listservers.html' , context_dict)
+    return render(request, 'psw/listservers.html', context_dict)
+
 
 def services(request):
-    
     if request.method == 'POST':
         qs = Commands.objects.filter(user=request.user)
-        form = ServicesForm(request.POST,user=request.user)
-        
+        form = ServicesForm(request.POST, user=request.user)
+
         if form.is_valid():
-            
             sql = form.cleaned_data['sql']
             http = form.cleaned_data['http']
             php = form.cleaned_data['php']
             form.save()
             return HttpResponseRedirect('services/')
-            
+
     else:
         form = ServicesForm()
     return render(request, 'psw/services.html', {'form': form})
-
